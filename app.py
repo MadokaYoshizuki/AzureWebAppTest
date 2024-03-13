dd1be548bcd1a400bde9a6a822d82804f2a0a79c
Madoka <madoka.yoshizuki@fosterlink.co.jp>
2024-03-13 12:06:38 +0900

    20240313

--- app.py
+++ app.py
@@ -1,32 +1,59 @@
-import os
+# import dash
+# import dash_bootstrap_components as dbc
+import plotly.express as px
+from dash import Dash, dcc, html, Input, Output, callback
+from flask import Flask
+import pandas as pd
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
 
-app = Flask(__name__)
+# flask_server = Flask(__name__)
+# app = dash.Dash(__name__, server=flask_server, external_stylesheets=[dbc.themes.BOOTSTRAP])
+# server = app.server
+app = Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Graph(id='graph-with-slider'),
+    dcc.Slider(
+        df['year'].min(),
+        df['year'].max(),
+        step=None,
+        value=df['year'].min(),
+        marks={str(year): str(year) for year in df['year'].unique()},
+        id='year-slider'
+    )
+])
 
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+@callback(
+    Output('graph-with-slider', 'figure'),
+    Input('year-slider', 'value'))
+def update_figure(selected_year):
+    filtered_df = df[df.year == selected_year]
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
+                     size="pop", color="continent", hover_name="country",
+                     log_x=True, size_max=55)
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+    fig.update_layout(transition_duration=500)
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    return fig
 
 
 if __name__ == '__main__':
-   app.run()
+    # app.run(debug=True)
+    app.run(debug=True, port="8000")
+
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+
+
+# if __name__ == '__main__':
+#     app.run()
